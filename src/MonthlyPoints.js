e57f885dd506088d099d9f9158377a575af8b68a
Alan Chiu <ms0372631@gmail.com>
2022-08-09 13:41:17 -0700

    monthly points

--- src/MonthlyPoints.js
+++ src/MonthlyPoints.js
@@ -1,4 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 
-import { getTransactions, getStart}
+import { getTransactions, getStartDate } from './api';
+import { PointCard } from './ScoreCard';
+
+export const MonthlyScores = ({ count }) => {
+    const [months, setMonths] = useState([])
+    const classes = useClasses()
+
+    useEffect(() => {
+        getTransactionsSince(getStartDate(count)).then(setMonths)
+    }, [count])
+
+    return (
+        <div className={classes.container}>
+            {months.map(([month, transactions]) =>
+                <ScoreCard title={month} transactions={transactions} key={month} />
+            )}
+        </div>
+    )
+}
